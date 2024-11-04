b033ae5f383240c8da17ec198f49cb21a0e6acc0
connor <kearnsco@mail.uc.edu>
2024-11-04 08:52:59 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int num1 = 15;
+        int num2 = 22;
+
+        if(num1 % 2 ==0)
+        {
+            System.out.println(num1 + " is even");
+        }
+        else
+        {
+            System.out.println(num1 + " is odd");
+        }
+        if(num2 % 2 ==0)
+        {
+            System.out.println(num2 + " is even");
+        }
+        else
+        {
+            System.out.println(num2 + " is odd");
+        }
+    }
+}
